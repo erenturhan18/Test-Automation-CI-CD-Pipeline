ebf9a208e8786b88509060330a1ab58e57ca9431
erenturhan18 <erenturhan120@gmail.com>
2026-03-11 17:08:43 +0300

    Amazon Test

---
+++ src/test/java/tests/AmazonSearchTest.java
@@ -0,0 +1,27 @@
+package tests;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.Driver;
+
+public class AmazonSearchTest {
+
+    @Test
+    public void amazonAramaTesti() {
+
+        Driver.getDriver().get("https://www.amazon.com");
+        System.out.println("Amazon ana sayfa acildi.");
+
+
+        WebElement aramaKutusu = Driver.getDriver().findElement(By.name("field-keywords"));
+        aramaKutusu.sendKeys("Nutella" + Keys.ENTER);
+        System.out.println("Arama yapildi: Nutella");
+
+        Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("Nutella"));
+
+        System.out.println("Test Basarili: Sonuc yazisinda Nutella goruldu.");
+        Driver.closeDriver();
+    }
+}
